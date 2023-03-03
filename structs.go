56ec615d7b655029d41063fed57320502f2d60a8
patrickjoh <patrick.johannessen@hotmail.com>
2023-03-03 16:47:31 +0100

    add: Added neccessary fields for processing a neighbour countries

--- structs.go
+++ structs.go
@@ -1,14 +1,14 @@
 package university_search
 
-// Response Diagnosis Struct for responding to the client
+// Diagnosis Struct for responding to the client
 type Diagnosis struct {
-	UniversitiesAPI string `json:"universitiesapi"` // "<http status code for universities API>",
-	CountriesAPI    string `json:"countriesapi"`    // "<http status code for restcountries API>",
-	Version         string `json:"v1"`              // "v1",
-	Uptime          string `json:"uptime"`          // "<time in seconds from the last service restart>"
+	UniversitiesAPI string `json:"universities_api"` // "<http status code for universities API>",
+	CountriesAPI    string `json:"countries_api"`    // "<http status code for rest_countries API>",
+	Version         string `json:"v1"`               // "v1",
+	Uptime          string `json:"uptime"`           // "<time in seconds from the last service restart>"
 }
 
-// Struct for storing data from HIPO Universities API
+// University Struct for storing data from Universities API
 type University struct {
 	IsoCode  string   `json:"alpha_two_code"`
 	WebPages []string `json:"web_pages"`
@@ -16,19 +16,20 @@ type University struct {
 	Country  string   `json:"country"`
 }
 
-// Struct for storing data from REST Countries API
+// Country Struct for storing data from REST Countries API
 type Country struct {
-	IsoCode   string            `json:"cca2"`
-	Languages map[string]string `json:"languages"`
-	Maps      map[string]string `json:"maps"`
-	Borders   []string          `json:"borders"`
+	IsoCode    string            `json:"cca2"`
+	Alpha3Code string            `json:"cca3"`
+	Languages  map[string]string `json:"languages"`
+	Maps       map[string]string `json:"maps"`
+	Border     []string          `json:"borders"`
 }
 
 // Response Struct for responding to the client
 type Response struct {
 	Name      string            `json:"name"`
 	Country   string            `json:"country"`
-	IsoCode   string            `json:"isocode"`
+	IsoCode   string            `json:"iso_code"`
 	WebPages  []string          `json:"webpages"`
 	Languages map[string]string `json:"languages"`
 	Maps      map[string]string `json:"maps"`
